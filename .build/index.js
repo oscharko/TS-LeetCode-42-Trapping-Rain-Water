365d1bcccaff65a2c8ee4e3079c22a5132f01b99
Oliver Scharkowski <59687448+oscharko@users.noreply.github.com>
2022-05-23 12:16:58 +0000

    optimize space complexity

--- .build/index.js
+++ .build/index.js
@@ -1,22 +1,22 @@
 let heights = [0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 2];
-const getTrappingRainWater = function(array) {
-  let totalWater = 0;
-  for (let p = 0; p < array.length; p++) {
-    let leftP = p, rightP = p, maxLeft = 0, maxRight = 0;
-    while (leftP >= 0) {
-      maxLeft = Math.max(maxLeft, array[leftP]);
-      leftP--;
+const getTrappingRainWater = function(a) {
+  let tW = 0;
+  for (let p = 0; p < a.length; p++) {
+    let v = [p, p, 0, 0];
+    while (v[0] >= 0) {
+      v[2] = Math.max(v[2], a[v[0]]);
+      v[0]--;
     }
-    while (rightP < array.length) {
-      maxRight = Math.max(maxRight, array[rightP]);
-      rightP++;
+    while (v[1] < a.length) {
+      v[3] = Math.max(v[3], a[v[1]]);
+      v[1]++;
     }
-    const currentWater = Math.min(maxLeft, maxRight) - array[p];
-    if (currentWater >= 0) {
-      totalWater += currentWater;
+    const cW = Math.min(v[2], v[3]) - a[p];
+    if (cW >= 0) {
+      tW += cW;
     }
   }
-  return totalWater;
+  return tW;
 };
 console.log(getTrappingRainWater(heights));
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,26 +1,46 @@
 let heights = [0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 2];
 
-const getTrappingRainWater = function (array: number[]): number {
-  let totalWater: number = 0;
-  for (let p: number = 0; p < array.length; p++) {
-    let leftP: number = p,
-      rightP: number = p,
-      maxLeft: number = 0,
-      maxRight: number = 0;
-    while (leftP >= 0) {
-      maxLeft = Math.max(maxLeft, array[leftP]);
-      leftP--;
+// const getTrappingRainWater = function (array: number[]): number {
+//   let totalWater: number = 0;
+//   for (let p: number = 0; p < array.length; p++) {
+//     let leftP: number = p,
+//       rightP: number = p,
+//       maxLeft: number = 0,
+//       maxRight: number = 0;
+//     while (leftP >= 0) {
+//       maxLeft = Math.max(maxLeft, array[leftP]);
+//       leftP--;
+//     }
+//     while (rightP < array.length) {
+//       maxRight = Math.max(maxRight, array[rightP]);
+//       rightP++;
+//     }
+//     const currentWater: number = Math.min(maxLeft, maxRight) - array[p];
+//     if (currentWater >= 0) {
+//       totalWater += currentWater;
+//     }
+//   }
+//   return totalWater;
+// };
+
+const getTrappingRainWater = function (a: number[]): number {
+  let tW: number = 0;
+  for (let p: number = 0; p < a.length; p++) {
+    let v: number[] = [p, p, 0, 0]
+    while (v[0] >= 0) {
+      v[2] = Math.max(v[2], a[v[0]]);
+      v[0]--;
     }
-    while (rightP < array.length) {
-      maxRight = Math.max(maxRight, array[rightP]);
-      rightP++;
+    while (v[1] < a.length) {
+      v[3] = Math.max(v[3], a[v[1]]);
+      v[1]++;
     }
-    const currentWater: number = Math.min(maxLeft, maxRight) - array[p];
-    if (currentWater >= 0) {
-      totalWater += currentWater;
+    const cW: number = Math.min(v[2], v[3]) - a[p];
+    if (cW >= 0) {
+      tW += cW;
     }
   }
-  return totalWater;
+  return tW;
 };
 
 console.log(getTrappingRainWater(heights));
