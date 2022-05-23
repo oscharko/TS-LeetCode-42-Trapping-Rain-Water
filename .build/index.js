0b0d965c1b8d2634141f5425679e26c0353c3d97
Oliver Scharkowski <59687448+oscharko@users.noreply.github.com>
2022-05-23 13:00:59 +0000

    optimise time complexity (TwoPointer)

--- .build/index.js
+++ .build/index.js
@@ -1,22 +1,24 @@
 let heights = [0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 2];
 const getTrappingRainWater = function(a) {
-  let tW = 0;
-  for (let p = 0; p < a.length; p++) {
-    let v = [p, p, 0, 0];
-    while (v[0] >= 0) {
-      v[2] = Math.max(v[2], a[v[0]]);
-      v[0]--;
-    }
-    while (v[1] < a.length) {
-      v[3] = Math.max(v[3], a[v[1]]);
-      v[1]++;
-    }
-    const cW = Math.min(v[2], v[3]) - a[p];
-    if (cW >= 0) {
-      tW += cW;
+  let v = [0, a.length - 1, 0, 0, 0];
+  while (v[0] < v[1]) {
+    if (a[v[0]] <= a[v[1]]) {
+      if (a[v[0]] >= v[2]) {
+        v[2] = a[v[0]];
+      } else {
+        v[4] += v[2] - a[v[0]];
+      }
+      v[0]++;
+    } else {
+      if (a[v[1]] >= v[3]) {
+        v[3] = a[v[1]];
+      } else {
+        v[4] += v[3] - a[v[1]];
+      }
+      v[1]--;
     }
   }
-  return tW;
+  return v[4];
 };
 console.log(getTrappingRainWater(heights));
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -23,24 +23,46 @@ let heights = [0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 2];
 //   return totalWater;
 // };
 
+// const getTrappingRainWater = function (a: number[]): number {
+//   let tW: number = 0;
+//   for (let p: number = 0; p < a.length; p++) {
+//     let v: number[] = [p, p, 0, 0]
+//     while (v[0] >= 0) {
+//       v[2] = Math.max(v[2], a[v[0]]);
+//       v[0]--;
+//     }
+//     while (v[1] < a.length) {
+//       v[3] = Math.max(v[3], a[v[1]]);
+//       v[1]++;
+//     }
+//     const cW: number = Math.min(v[2], v[3]) - a[p];
+//     if (cW >= 0) {
+//       tW += cW;
+//     }
+//   }
+//   return tW;
+// };
+
 const getTrappingRainWater = function (a: number[]): number {
-  let tW: number = 0;
-  for (let p: number = 0; p < a.length; p++) {
-    let v: number[] = [p, p, 0, 0]
-    while (v[0] >= 0) {
-      v[2] = Math.max(v[2], a[v[0]]);
-      v[0]--;
-    }
-    while (v[1] < a.length) {
-      v[3] = Math.max(v[3], a[v[1]]);
-      v[1]++;
-    }
-    const cW: number = Math.min(v[2], v[3]) - a[p];
-    if (cW >= 0) {
-      tW += cW;
+  let v: number[] = [0, (a.length - 1), 0, 0, 0]
+  while (v[0] < v[1]) {
+    if (a[v[0]] <= a[v[1]]) {
+      if (a[v[0]] >= v[2]) {
+        v[2] = a[v[0]]
+      } else {
+        v[4] += v[2] - a[v[0]]
+      }
+      v[0]++
+    } else {
+      if (a[v[1]] >= v[3]) {
+        v[3] = a[v[1]]
+      } else {
+        v[4] += v[3] - a[v[1]]
+      }
+      v[1]--
     }
   }
-  return tW;
+  return v[4];
 };
 
 console.log(getTrappingRainWater(heights));
